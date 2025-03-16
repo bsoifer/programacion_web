698a469c96d310c11cfce64d643321f82e97ba42
bsoifer <bsoifer@itba.edu.ar>
2025-03-16 17:39:11 -0300

    first commit

---
+++ comments.js
@@ -0,0 +1,21 @@
+// node --> para usdar js
+// .exit --> para salir de js y volver a powershell
+
+/*[
+    {
+        "key": "ctrl+enter",
+        "command": "workbench.action.terminal.runSelectedText",
+        "when": "editorTextFocus && editorHasSelection"
+    },
+    {
+        "key": "ctrl+enter",
+        "command": "editor.action.insertLineAfter",
+        "when": "editorTextFocus && !editorHasSelection"
+    },
+    {
+        "key": "ctrl+enter",
+        "command": "workbench.action.terminal.runSelectedText",
+        "when": "editorTextFocus && !editorHasSelection",
+        "args": {"text": "${lineNumber}"}
+    }
+] */
