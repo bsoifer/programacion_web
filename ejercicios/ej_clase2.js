0682c3eb3d07ccd4c4dbe3514ed7184ce2e8508d
bsoifer <bsoifer@itba.edu.ar>
2025-03-22 20:38:30 -0300

    clase 2 terminada

--- ejercicios/ej_clase2.js
+++ ejercicios/ej_clase2.js
@@ -91,3 +91,140 @@ const peliculas = [
 ]
 
 console.log(peliculas[peliculas.length-1].director)
+
+
+/* 6. Ejercicio 6: Operadores Lógicos
+Declara tres variables booleanas y usa operadores lógicos para verificar si al menos dos
+son verdaderas. Imprime true o false en la consola dependiendo del resultado.
+*/
+
+let booleana1 = true
+let booleana2 = true
+let booleana3 = false
+
+if((booleana1 === true && booleana2 === true) || (booleana1 === true && booleana3 === true) || (booleana2 === true && booleana3 === true)){
+  console.log("Al menos dos variables son true")
+}else{
+  console.log("Al menos dos variables son false")
+}
+
+/* 7. Ejercicio 7: Comparaciones
+Declara tres variables numéricas. Compara si el primer número es mayor que el segundo y
+menor que el tercero. Luego, verifica si el segundo número es distinto al tercero. Imprime
+los resultados de las comparaciones.
+*/
+
+let var1 = 15
+let var2 = 10
+let var3 = 100
+
+if(var1 > var2 && var1 < var3){
+  if(var2 !== var3){
+    console.log("Se cumplen las condiciones")
+  }else{
+    console.log("El tercer número es igual al segundo")
+  }
+}else{
+  console.log("El segundo número es mayor o igual al primero, ó el primer número es mayor o igual al tercero")
+}
+
+/* 8. Ejercicio 8: Concatenación de Strings
+Declara dos variables que almacenen strings (por ejemplo, tu nombre y tu apellido). Luego,
+crea una tercera variable que concatene las dos primeras y muestra el resultado en la
+consola.
+*/
+
+let nombre = "Estaban"
+let apellido = "Quito"
+let nombreApellido = nombre + " " + apellido
+
+console.log("Nombre y apellido: " + nombreApellido)
+
+/* 9. Ejercicio 9: Incremento y Decremento
+Declara una variable numérica y utiliza los operadores de incremento (++) y decremento
+(--) para modificar su valor. Imprime el valor de la variable después de cada operación.
+*/
+
+let varNumerica = 0;
+
+// llevarla de a uno hasta diez
+console.log(varNumerica);
+for(let i = 1; i <= 10; i++){
+  varNumerica++;
+  console.log(varNumerica);
+}
+
+//llevarla de nuevo a cero de a uno
+console.log(varNumerica);
+for(let i = 1; i <= 10; i++){
+  varNumerica--;
+  console.log(varNumerica);
+}
+
+/* 10. Ejercicio 10: Propiedades de Objetos
+Crea un objeto que represente un estudiante con propiedades como nombre, edad y
+carrera. Accede e imprime cada propiedad del objeto.
+*/
+
+let estudiante = {
+  nombre: "Esteban",
+  apellido: "Quito",
+  edad: 23,
+  carrera: "analitica"
+}
+
+console.log(estudiante.nombre)
+console.log(estudiante["carrera"])
+
+/*
+11. Ejercicio 11: Arrays Multidimensionales
+Crea un array que contenga dos arrays internos, cada uno con 3 números. Accede al
+segundo elemento del primer array interno.
+*/
+
+let ejercicioOnce = [[1,6,3],[30,6,34]]
+
+console.log("El segundo elemento del primer array es: " + ejercicioOnce[0][1])
+
+
+/* 12. Ejercicio 12: Operadores Relacionales
+Declara dos variables numéricas y utiliza los operadores >, <, >=, <=, ===, !== para
+comparar sus valores. Imprime el resultado de cada comparación.
+*/
+
+let variable1 = 10
+let variable2 = 32
+
+variable1 > variable2
+variable1 < variable2
+variable1 >= variable2
+variable1 <= variable2
+variable1 === variable2
+variable1 !== variable2
+
+/*
+13. Ejercicio 13: Uso de typeof con Variables No Definidas
+Declara una variable sin asignarle un valor y otra variable con valor null. Utiliza typeof
+para imprimir el tipo de cada variable.
+*/
+
+let variableSinValor
+let variableNull = null
+
+typeof(variableSinValor)
+typeof(variableNull)
+
+
+/*
+14. Ejercicio 14: Conversión de Tipos
+Declara una variable que almacene un número como string (por ejemplo, "123"). Convierte
+este string a un número usando parseInt o Number y demuestra que ahora puedes
+realizar operaciones matemáticas con él.
+*/
+
+let numeroString = "100"
+numeroString = Number(numeroString)
+
+numeroString + 5
+
+// Páginas de ejercicios a leer: 8, 9, 13, 14
